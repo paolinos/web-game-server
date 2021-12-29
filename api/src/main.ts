0fc8df60c43f130f22dd5199ec9b71a3a05fdc03
Pablo C <pendlejin@yahoo.com.ar>
2021-12-29 12:33:10 +0100

    #api Add swagger to api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+
 import { AppModule } from './app.module';
+
 
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
   const port = 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Api - Game')
+    .setDescription("Not much to say....")
+    .setVersion("0.0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  writeFileSync("./swagger-spec.json", JSON.stringify(document));
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(port, () => {
     console.log(`App listening at port: ${port}`);
   });
