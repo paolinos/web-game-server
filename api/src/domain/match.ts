abb6ace0445f6d9f7267241ac76235b529349db4
Pablo C <pendlejin@yahoo.com.ar>
2021-12-30 10:02:47 +0100

    #api Add User & MAtch entities

---
+++ api/src/domain/match.ts
@@ -0,0 +1,23 @@
+import { IdEntity } from './id.entity';
+import { User } from "./user";
+import { v4 as uuidv4 } from 'uuid';
+
+export class Match implements IdEntity{
+    public id:string;
+
+    public users:User[] = [];
+
+    public status:MatchStatus = MatchStatus.STAND_BY;
+
+    constructor(){
+        // NOTE: this is only an example. But uuidv4() SHOULD NOT BE HERE
+        this.id = uuidv4();
+    }
+}
+
+export enum MatchStatus{
+    STAND_BY = 0,
+    WAITING_PLAYERS = 1,
+    PLAYING = 2,
+    END_GAME = 3
+}
---
+++ api/src/domain/user.ts
@@ -0,0 +1,28 @@
+import { IdEntity } from './id.entity';
+import { v4 as uuidv4 } from 'uuid';
+
+export class User implements IdEntity{
+    public id:string;
+
+    public email:string;
+
+    public lastSigin:Date;
+
+    public points:number = 0;
+
+    public status:UserStatus = UserStatus.DEFAULT;
+
+    constructor(email:string){
+        // NOTE: this is only an example. But uuidv4() SHOULD NOT BE HERE 
+        this.id = uuidv4();
+        
+        this.email = email;
+        this.lastSigin = new Date();
+    }
+}
+
+export enum UserStatus{
+    DEFAULT = 0,
+    SEARCHING = 1,
+    PLAYING = 2
+}
