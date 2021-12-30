5b99bcc3a9fd0666dd8eb7812aea1ab2b6474bd7
Pablo C <pendlejin@yahoo.com.ar>
2021-12-30 10:11:39 +0100

    #api Update app.module to import RepositoyModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './core/auth/auth.module';
 import { GameModule } from './core/game/game.module';
 import { UserModule } from './core/user/user.module';
-
+import { RepositoryModule } from './repositories/repository.module';
 
 @Module({
   imports: [
+    RepositoryModule,
     AuthModule,
     GameModule,
-    UserModule
+    UserModule,
   ],
   controllers: [],
   providers: [],
