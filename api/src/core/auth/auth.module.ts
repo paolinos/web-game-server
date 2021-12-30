c8dd7d8fdcb837721e1a38c53196d9eceece7bdc
Pablo C <pendlejin@yahoo.com.ar>
2021-12-30 10:12:21 +0100

    #api Update auth.module to userUserRepository as provider

--- api/src/core/auth/auth.module.ts
+++ api/src/core/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserRepository } from '../../repositories/user.repository';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserRepository],
 })
 export class AuthModule {}
