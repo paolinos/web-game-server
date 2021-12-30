e5176026a273a7cfc332b207f3c44366c58ddf55
Pablo C <pendlejin@yahoo.com.ar>
2021-12-30 10:58:20 +0100

    #api Refactor move jwt verify/sign to jwt.helper file

---
+++ api/src/common/auth/jwt.helper.ts
@@ -0,0 +1,37 @@
+import { sign, verify } from 'jsonwebtoken';
+
+const JWT_SECRET = "temp-secret"
+
+/**
+ * 
+ * @param token string
+ * @param ignoreExpiration (optional) default false 
+ * @returns object or null
+ */
+export const parseToken = (token:string, ignoreExpiration:boolean=false):object|null => {
+    try {
+        const result:any = verify(token, JWT_SECRET, {ignoreExpiration});
+        if(result){
+            return result.data;
+        }
+    } catch (error) {
+        // NOTE: log in console
+        console.error(`parseToken ERROR: token: ${token} => `,error);
+    }
+    return null;
+}
+
+/**
+ * Generate token from object
+ * @param data object to encrypt into jwt
+ * @returns string
+ */
+export const generateToken = (data:object) : string => {
+    return sign(
+        {
+            exp: Math.floor(Date.now() / 1000) + (60 * 60),
+            data,
+        }, 
+        JWT_SECRET
+    );
+}
