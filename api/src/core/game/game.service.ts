3817bd3ce6367f6b6d9fcf12fe9ac3a036798218
Pablo C <pendlejin@yahoo.com.ar>
2022-01-04 18:54:20 +0100

    #api Update game.service to inject the appRabbitService and send event

--- api/src/core/game/game.service.ts
+++ api/src/core/game/game.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 import { UserRepository } from '../../repositories/user.repository';
 import { UserStatus } from '../../domain/user';
+import { APP_RABBIT_SERVICE } from './game.const';
+import { SEARCH_GAME_EVENT } from '../../microservice/rabbitmq/events';
+
 
 
 @Injectable()
 export class GameService {
 
     constructor(
-		private readonly userRepository:UserRepository
+		private readonly userRepository:UserRepository,
+		@Inject(APP_RABBIT_SERVICE) private appRabbitService: ClientProxy
 	){}
 
 	async searchGame(email:string):Promise<boolean>  {
@@ -17,9 +22,11 @@ export class GameService {
 		if(user){
 			if(user.status === UserStatus.DEFAULT){
 				user.status = UserStatus.SEARCHING;
+				
+				// TODO:
+				//	 review Transactional outbox pattern or something similar
 				await this.userRepository.save(user);
-
-				// TODO: send message
+				await this.appRabbitService.emit(SEARCH_GAME_EVENT, { userId: user.id, username: user.email });
 
 				return true;	
 			}
