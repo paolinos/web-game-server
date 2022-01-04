ca96a838ac4726915d7ea01eec747d7a71ba1eb8
Pablo C <pendlejin@yahoo.com.ar>
2022-01-04 18:52:11 +0100

    #api Update game.module create and import createRabbitModule

--- api/src/core/game/game.module.ts
+++ api/src/core/game/game.module.ts
@@ -3,9 +3,15 @@ import { Module } from '@nestjs/common';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
 import { RepositoryModule } from '../../repositories/repository.module';
+import { createRabbitModule } from '../../microservice/rabbitmq/rabbit.module';
+import { RABBIT_QUEUE_APP } from '../../consts';
+import { APP_RABBIT_SERVICE } from './game.const';
 
 @Module({
-	imports: [RepositoryModule],
+	imports: [
+		RepositoryModule,
+		createRabbitModule(APP_RABBIT_SERVICE, RABBIT_QUEUE_APP)
+	],
 	controllers: [GameController],
 	providers: [GameService],
 })
