2a6edaf8e829d38555c7104b5dc998c50c06c23a
Pablo C <pendlejin@yahoo.com.ar>
2022-01-04 15:13:34 +0000

    #api Update game.controller to use AuthGuard

--- api/src/core/game/game.controller.ts
+++ api/src/core/game/game.controller.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Controller, HttpCode, HttpStatus, Put } from '@nestjs/common';
+import { BadRequestException, Controller, HttpCode, HttpStatus, Put, UseGuards, Request, UnauthorizedException } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { GameService } from './game.service';
+import { AuthGuard } from '../../common/auth/auth.guard';
 
 @ApiTags('Game')
 @Controller('game')
@@ -8,11 +9,15 @@ export class GameController {
 	constructor(private readonly gameService: GameService) {}
 
 	@ApiBearerAuth()
+	@UseGuards(AuthGuard)
 	@HttpCode(HttpStatus.ACCEPTED)
 	@Put()
-	async searchGame(): Promise<void> {
+	async searchGame(@Request() res): Promise<void> {
+		if(!res.scope){
+			throw new UnauthorizedException();
+		}
 
-		const result = await this.gameService.searchGame("jwt.email");
+		const result = await this.gameService.searchGame(res.scope.user.email);
 
 		if(!result){
 			throw new BadRequestException();
