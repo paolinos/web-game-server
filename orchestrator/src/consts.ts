5c9e1686fc38605106e134141a238a4c0e5178fe
Pablo C <pendlejin@yahoo.com.ar>
2022-01-05 15:53:50 +0100

    #orchestrator Add consts with Rabbit configurations

---
+++ orchestrator/src/consts.ts
@@ -0,0 +1,7 @@
+//  RabbitMQ
+export const RABBIT_HOST:string="localhost";
+export const RABBIT_PORT:number=5672;
+
+export const RABBIT_QUEUE_APP:string="game-queue-app";
+export const RABBIT_QUEUE_ORCHESTRATOR:string="game-queue-orchestrator";
+export const RABBIT_QUEUE_MATCH:string="game-queue-match";
