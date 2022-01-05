1c7ec3ff1e60ef89db1d18f3e3e2fcb1f8be9066
Pablo C <pendlejin@yahoo.com.ar>
2022-01-05 17:15:24 +0100

    #orchestrator Update consts with MatchStatus

--- orchestrator/src/consts.ts
+++ orchestrator/src/consts.ts
@@ -5,3 +5,11 @@ export const RABBIT_PORT:number=5672;
 export const RABBIT_QUEUE_APP:string="game-queue-app";
 export const RABBIT_QUEUE_ORCHESTRATOR:string="game-queue-orchestrator";
 export const RABBIT_QUEUE_MATCH:string="game-queue-match";
+
+
+export enum MatchStatus{
+    STAND_BY = 0,
+    WAITING_PLAYERS = 1,
+    PLAYING = 2,
+    END_GAME = 3
+}
