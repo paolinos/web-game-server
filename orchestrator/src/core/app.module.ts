a985c1656c201ff041263c16fc13fd7e7084ea42
Pablo C <pendlejin@yahoo.com.ar>
2022-01-05 17:29:48 +0100

    #orchestrator Add app.const and Update app.module with rabbit service

--- orchestrator/src/core/app.module.ts
+++ orchestrator/src/core/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createRabbitModule } from '../microservice/rabbitmq/rabbit.module';
+import { APP_RABBIT_SERVICE } from './app.const';
+import { RABBIT_QUEUE_ORCHESTRATOR } from '../consts'; 
 
 @Module({
+    imports: [
+		createRabbitModule(APP_RABBIT_SERVICE, RABBIT_QUEUE_ORCHESTRATOR)
+	],
     controllers: [AppController],
     providers: [AppService],
 })
