ea6159fa52741fe910dde1c3dffe8b2239acde89
Pablo C <pendlejin@yahoo.com.ar>
2022-01-08 18:45:11 +0100

    #match Update rabbitmq events, start to use a differnt CREATED_MATCH event (CREATED_MATCH_SERVER_EVENT)

--- match/src/microservice/rabbitmq/events.ts
+++ match/src/microservice/rabbitmq/events.ts
@@ -1,5 +1,5 @@
 // Match Queue
-export const CREATED_MATCH_EVENT:string = "created-match-event";
+export const CREATED_MATCH_SERVER_EVENT:string = "created-match-server-event";
 export const BEGUN_MATCH_EVENT:string = "begun-match-event";
 export const END_MATCH_EVENT:string = "end-match-event";
 export const ERROR_MATCH_EVENT:string = "error-match-event";
@@ -7,4 +7,4 @@ export const ERROR_MATCH_EVENT:string = "error-match-event";
 // Notify new Match available to run as host
 export const INIT_MATCH_CONTAINER_EVENT:string = "init-match-container-event";
 // Notify Match is shutting down
-export const END_MATCH_CONTAINER_EVENT:string = "END-match-container-event";
+export const END_MATCH_CONTAINER_EVENT:string = "end-match-container-event";
