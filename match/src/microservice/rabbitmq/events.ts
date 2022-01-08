21aaf83351bbc01e7456d547c08d11a7ec587509
Pablo C <pendlejin@yahoo.com.ar>
2022-01-08 17:01:37 +0100

    #match Add RabbitMQ event & rabbit.module

---
+++ match/src/microservice/rabbitmq/events.ts
@@ -0,0 +1,10 @@
+// Match Queue
+export const CREATED_MATCH_EVENT:string = "created-match-event";
+export const BEGUN_MATCH_EVENT:string = "begun-match-event";
+export const END_MATCH_EVENT:string = "end-match-event";
+export const ERROR_MATCH_EVENT:string = "error-match-event";
+
+// Notify new Match available to run as host
+export const INIT_MATCH_CONTAINER_EVENT:string = "init-match-container-event";
+// Notify Match is shutting down
+export const END_MATCH_CONTAINER_EVENT:string = "END-match-container-event";
