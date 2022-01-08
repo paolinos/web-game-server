987357896f0c6e3460ed61db1cd0b88e28330547
Pablo C <pendlejin@yahoo.com.ar>
2022-01-08 12:36:04 +0100

    #api Update app.module with MatchModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,14 +3,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './core/auth/auth.module';
 import { GameModule } from './core/game/game.module';
 import { UserModule } from './core/user/user.module';
+import { MatchModule } from './core/match/match.module';
 
 @Module({
   imports: [
     AuthModule,
     GameModule,
     UserModule,
+    MatchModule
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
