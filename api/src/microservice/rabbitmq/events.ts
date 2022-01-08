847531d83bee229d9b7684cdbc545909002d9019
Pablo C <pendlejin@yahoo.com.ar>
2022-01-08 12:30:57 +0100

    #api Update RabbitMQ events

--- api/src/microservice/rabbitmq/events.ts
+++ api/src/microservice/rabbitmq/events.ts
@@ -1,1 +1,7 @@
+// App Queue
 export const SEARCH_GAME_EVENT:string = "search-game-event";
+
+// Match Queue
+export const CREATED_MATCH_EVENT:string = "created-match-event";
+export const BEGUN_MATCH_EVENT:string = "begun-match-event";
+export const END_MATCH_EVENT:string = "end-match-event";
