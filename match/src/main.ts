b1fcc29b6fe134b8f010fe024471d4e9146bf50e
Pablo C <pendlejin@yahoo.com.ar>
2022-01-14 20:26:19 +0100

    #match Update Main to load MainController to make Websocket running

--- match/src/main.ts
+++ match/src/main.ts
@@ -1,20 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
 
 import { RABBIT_QUEUE_MATCH } from './consts';
 import { createRabbitOptions } from './microservice/rabbitmq/rabbit.module';
-import { MatchModule } from './core/match/match.module';
+import { AppModule } from './app.module';
+
+const MATCH_PORT:number = 8000;
+const GAME_NAME:string = "PONG";
 
 async function bootstrap() {
-  //--------------------------------------------------------
-	//	"Microservices"
+	const app = await NestFactory.create(AppModule, { cors: true });
+	app.connectMicroservice(createRabbitOptions(RABBIT_QUEUE_MATCH));
 
-  //  RabbitMQ Consumer - RABBIT_QUEUE_MATCH queue
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    MatchModule,
-    createRabbitOptions(RABBIT_QUEUE_MATCH)
-  );
-
-  await app.listen();
+	await app.startAllMicroservices();
+	await app.listen(MATCH_PORT, () => {
+		console.log(`Match running at port: ${MATCH_PORT}`)
+	});
 }
 bootstrap();
