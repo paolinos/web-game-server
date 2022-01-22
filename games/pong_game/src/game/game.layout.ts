b6688fb23be20d62a1983cd7ce25c338d36149c5
Pablo C <pendlejin@yahoo.com.ar>
2022-01-22 19:43:19 +0100

    #pong-game Add game.layout.ts to have all the rendereable object from layout

---
+++ games/pong_game/src/game/game.layout.ts
@@ -0,0 +1,50 @@
+import { Text } from "pixi.js";
+import { createLineGraphics, createText } from "../render/render.help";
+import { HALF_HEIGHT, HALF_WIDTH, HEIGHT, Point, WIDTH } from "./base";
+import { RenderableObject } from "./renderable.object";
+
+
+class PlayerLayout extends RenderableObject {
+    
+    private readonly _name:Text;
+    private readonly _points:Text;
+
+    constructor(name:string){
+        super();
+
+        this._name = createText(name);
+        this._name.y = 20;
+        this._container.addChild(this._name);
+
+        this._points = createText(name);
+        this._points.y = 20;
+        this._container.addChild(this._points);
+    }
+
+    set point(value:number){
+        this._points.text = value.toString();
+    }
+}
+
+
+
+export class GameLayout extends RenderableObject {
+
+    private readonly _player1:PlayerLayout;
+    private readonly _player2:PlayerLayout;
+
+    constructor(name1:string, name2:string){
+        super();
+
+        this._player1 = new PlayerLayout(name1);
+        this._player1.setPos(HALF_WIDTH * 0.5, 20);
+        this._container.addChild(this._player1.container);
+
+        this._player2 = new PlayerLayout(name2);
+        this._player2.setPos(HALF_WIDTH + (HALF_WIDTH * 0.5), 20);
+        this._container.addChild(this._player2.container);
+
+        this._container.addChild(createLineGraphics({x: HALF_WIDTH, y:0 }, {x:HALF_WIDTH, y: HEIGHT}));
+        this._container.addChild(createLineGraphics({x: 0, y:HALF_HEIGHT }, {x:WIDTH, y: HALF_HEIGHT}));
+    }
+}
