022edb68487bafa4c25522f6d181854816e34185
Pablo C <pendlejin@yahoo.com.ar>
2022-01-22 19:36:47 +0100

    #pong-game Add render.help.ts to encapsulate some functions to create PIXI objects

---
+++ games/pong_game/src/render/render.help.ts
@@ -0,0 +1,50 @@
+import { Graphics, Text } from "pixi.js";
+import { Point } from "../game/base";
+
+/**
+ * Create a rectangle graphics
+ * @param {number} width 
+ * @param {number} height 
+ * @param {number} color  
+ * @param {number} x (optional)
+ * @param {number} y (optional)  
+ * @returns {PIXI.Graphics}
+ */
+export const createRectGraphics = (width:number, height:number, color:number=0xFFFFFF, x:number = 0, y:number = 0):Graphics => {
+    const graph = new Graphics();
+    graph.beginFill(color);
+    graph.drawRect(x, y, width, height);
+    graph.endFill();
+    return graph;
+}
+
+/**
+ * 
+ * @param {Point} from 
+ * @param {Point} to 
+ * @param {number} color
+ * @returns {PIXI.Graphics} 
+ */
+export const createLineGraphics = (from:Point, to:Point, color:number=0xFFFFFF):Graphics => {
+    const graph = new Graphics();
+    graph.lineStyle(1, color).moveTo(from.x, from.y).lineTo(to.x, to.y);
+    return graph;
+}
+
+/**
+ * Create default text
+ * @param {string} text 
+ * @param {number} color 
+ * @returns {PIXI.Text}
+ */
+export const createText = (text:string, color:number=0xFFFFFF):Text => {
+    return new Text(
+        text,
+        {
+            fontFamily: 'Arial', 
+            fontSize: 24, 
+            fill: color, 
+            align : 'center'
+        }
+    );
+}
