9a37143d9881e442253938b9ce148bc1524af9fa
Pablo C <pendlejin@yahoo.com.ar>
2022-01-28 18:38:26 +0100

    #pong-game Update main, to user SiginService & connect to match server with WS

--- games/pong_game/src/main.ts
+++ games/pong_game/src/main.ts
@@ -1,5 +1,7 @@
 import { PongGame } from './game/pong.game';
 import { Game } from './game/base';
+import { SigninService, SigninBusiness } from './services/signin.services';
+import { io } from "socket.io-client";
 
 // TODO: 
 //  - signin screen
@@ -13,6 +15,45 @@ import { Game } from './game/base';
 //      - send events by WS for movement
 //      - validate points in both sides
 
+const HOST:string = "localhost";
 
-const game:Game = new PongGame();
-game.start();
+// TODO: move this to sigin page/view
+const siginSrv:SigninService = new SigninBusiness();
+siginSrv.signin("some@email.com");
+
+
+// TODO: Move this to game page/view
+const socket = io(`ws://${HOST}:8000`, {
+    transports: ["websocket"],
+    path: '/match-id',
+    autoConnect: false,
+    auth: {
+       token:  siginSrv.token
+    }
+});
+
+let game:Game = null;
+const createGame = () => {
+    if(!game){
+        game = new PongGame();
+    }
+
+    socket.emit('ready');
+}
+
+
+socket.on("connect", () => {  
+    console.log("connect", socket.id);
+    createGame(); 
+});
+socket.on("connect_error", (e) => {
+    console.log("connect_error", e, socket);
+});
+socket.on("disconnect", () => {  
+    console.log("disconnect", socket.id); 
+});
+
+socket.on('start_game', (e:any) => {
+    console.log('start_game', e);
+})
+socket.connect();
