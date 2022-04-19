06fd99f03b17f48c1eac13b894d23f2f49008723
Pablo C <pendlejin@yahoo.com.ar>
2022-04-19 21:10:55 +0200

    #orchestrator Update 'Consumer' to use UserService

--- orchestrator/src/public/pubsub/consumers/index.ts
+++ orchestrator/src/public/pubsub/consumers/index.ts
@@ -1,5 +1,8 @@
+import { UserService } from "../../../application/interfaces/user.service.interface";
+import { UserBusinessLogic } from "../../../application/services/user.service";
 import { TOPIC } from "../topics";
 import { AppConsumer, MatchConsumer } from "./consumer";
+import ContextUnitOfWork from '../../../repositories/unitOfWork';
 
 interface SearchGameMessage {
    id: string,
@@ -12,11 +15,27 @@ interface MatchReadyMessage {
 }
 
 const app = new AppConsumer();
+const userService:UserService = new UserBusinessLogic(ContextUnitOfWork.UserRepository);
+
 app.subscribeTo(TOPIC.SEARCH_GAME, async (message:SearchGameMessage) => {
    console.log(`Message: ${TOPIC.SEARCH_GAME} data:`, message);
+
+   const result = await userService.searchGame(message.id, message.email);
+   if(!result.isValid()){
+      const errorMessage = "Error trying to Add User to search a game."
+      console.error(errorMessage, result.error);
+      throw new Error(errorMessage);
+   }
 });
 app.subscribeTo(TOPIC.CANCEL_SEARCH_GAME, async (message:SearchGameMessage) => {
    console.log(`Message: ${TOPIC.CANCEL_SEARCH_GAME} data:`, message);
+
+   const result = await userService.cancelSearchGame(message.id);
+   if(!result.isValid()){
+      const errorMessage = "Error trying to Remove User to search a game."
+      console.error(errorMessage, result.error);
+      throw new Error(errorMessage);
+   }
 })
 app.startToListenAsync();
 
