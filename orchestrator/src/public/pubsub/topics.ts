b8b68974e71ba35116317a0f842eedddb761f418
Pablo C <pendlejin@yahoo.com.ar>
2022-04-21 19:43:09 +0200

    #orchestrator Updates topics

--- orchestrator/src/public/pubsub/topics.ts
+++ orchestrator/src/public/pubsub/topics.ts
@@ -4,9 +4,13 @@ export enum TOPIC {
     CANCEL_SEARCH_GAME = "topic-cancel-search-game",
 
     CREATED_MATCH = "topic-created-match",
+    // MATCH_FORCE_RESTART => Cancel match, forcing restart the app, and be ready to host a new match 
+    FORCE_RESTART_MATCH = "topic-force-restart-match",
 
+    //  MATCH_READY -> after match start or after end the match, when is ready to host new match will send this message
     MATCH_READY = "topic-match-ready",
     MATCH_INIT = "topic-match-init",
     MATCH_END = "topic-match-end",
-    MATCH_ERROR = "topic-match-error",
+    // MATCH_ERROR => match notify the error that can have.
+    MATCH_ERROR = "topic-match-error"
 }
