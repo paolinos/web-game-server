ba998999c0891bd40ee6f4b92d343fe123aa2a3c
Pablo C <pendlejin@yahoo.com.ar>
2022-04-24 11:01:42 +0200

    #orchestrator Add new RABBIT_QUEUE_ORCHESTRATOR const

--- orchestrator/src/consts.ts
+++ orchestrator/src/consts.ts
@@ -1,14 +1,15 @@
 //  RabbitMQ
-export const RABBIT_HOST:string="localhost";
-export const RABBIT_PORT:number=5672;
+export const RABBIT_HOST = 'localhost';
+export const RABBIT_PORT = 5672;
+export const RABBIT_DURABLE = false;
 
-export const RABBIT_QUEUE_APP:string="game-queue-app";
-export const RABBIT_QUEUE_MATCH:string="game-queue-match";
+export const RABBIT_QUEUE_APP = 'game-queue-app';
+export const RABBIT_QUEUE_ORCHESTRATOR = 'game-queue-orchestrator';
+export const RABBIT_QUEUE_MATCH = 'game-queue-match';
 
-
-export enum MatchStatus{
-    STAND_BY = 0,
-    WAITING_PLAYERS = 1,
-    PLAYING = 2,
-    END_GAME = 3
+export enum MatchStatus {
+  STAND_BY = 0,
+  WAITING_PLAYERS = 1,
+  PLAYING = 2,
+  END_GAME = 3,
 }
